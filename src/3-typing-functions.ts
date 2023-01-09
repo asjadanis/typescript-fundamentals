fbff0ded339e306e05af3ab864564adacf110bc1
asjadanis <asjadanis128@gmail.com>
2023-01-09 12:43:56 -0600

    feat: typing functions exercise

---
+++ src/3-typing-functions.ts
@@ -0,0 +1,9 @@
+function addNumbers2(a: number, b: number): number {
+  return a + b;
+}
+
+addNumbers2(addNumbers2(3, 4), 4);
+addNumbers2(addNumbers2(3, 4), 4);
+addNumbers2(addNumbers2(3, 4), 4);
+addNumbers2(addNumbers2(3, 4), 4);
+addNumbers2(addNumbers2(3, 4), 4);
